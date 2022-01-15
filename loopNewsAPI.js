d1b276bec5aac655b5512ff6f5974e6d3b46fc69
AMY8988 <amymoe2321@gmail.com>
2022-01-15 13:01:25 +0630

    swd

---
+++ loopNewsAPI.js
@@ -0,0 +1,267 @@
+let data =
+{
+    "status": "ok",
+    "totalResults": 15039,
+    "articles": [
+    {
+    "source": {
+    "id": null,
+    "name": "Electrek"
+    },
+    "author": "Michelle Lewis",
+    "title": "Elon Musk says California proposal to cut solar incentives is a ‘bizarre anti-environment move’",
+    "description": "Late Tuesday evening, Tesla CEO Elon Musk weighed in on a controversial topic in California: the state’s solar tax proposal. Musk tweeted that California’s proposal was a “bizarre anti-environment move by govt of California.”\n more…\nThe post Elon Musk says Ca…",
+    "url": "https://electrek.co/2022/01/12/elon-musk-says-california-proposal-to-cut-solar-incentives-is-a-bizarre-anti-environment-move/",
+    "urlToImage": "https://i0.wp.com/electrek.co/wp-content/uploads/sites/3/2019/01/Tesla-Solar-Roof-hero-e1546911751570.jpeg?resize=1200%2C628&quality=82&strip=all&ssl=1",
+    "publishedAt": "2022-01-12T16:04:05Z",
+    "content": "Late Tuesday evening, Tesla CEO Elon Musk weighed in on a controversial topic in California: the state’s solar tax proposal. Musk tweeted that California’s proposal was a “bizarre anti-environment mo… [+4081 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "AutoExpress"
+    },
+    "author": "Steve Fowler",
+    "title": "'Sony's arrival as a car maker could be the most exciting in ages'",
+    "description": "Steve Fowler thinks Sony could hit the ground running in the electric car market",
+    "url": "https://www.autoexpress.co.uk/opinion/357084/sonys-arrival-car-maker-could-be-most-exciting-ages",
+    "urlToImage": "https://media.autoexpress.co.uk/image/private/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1642002406/autoexpress/2022/01/Sony opinion.jpg",
+    "publishedAt": "2022-01-12T16:03:32Z",
+    "content": "What does Sony mean to you? The days when it was king of the hill in the audio and TV worlds are long gone, but most of us on the Auto Express team remember Walkmans, TVs and even Sony mobile phones … [+1745 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Electrek"
+    },
+    "author": "Jo Borras",
+    "title": "BMW Group posts strong numbers, more than doubles plug-in sales in 2021",
+    "description": "The BMW Group had an incredibly successful 2021, with its BMW and Rolls-Royce brands posting record sales. The big news, however, is on the electric side, with sales of battery EVs from BMW and MINI more than doubling over 2020.\n more…\nThe post BMW Group post…",
+    "url": "https://electrek.co/2022/01/12/bmw-group-posts-strong-numbers-more-than-doubles-plug-in-sales-in-2021/",
+    "urlToImage": "https://i2.wp.com/electrek.co/wp-content/uploads/sites/3/2022/01/BMW-330e-plug-in-hybrid_MAIN.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1",
+    "publishedAt": "2022-01-12T16:00:00Z",
+    "content": "The BMW Group had an incredibly successful 2021, with its BMW and Rolls-Royce brands posting record sales. The big news, however, is on the electric side, with sales of battery EVs from BMW and MINI … [+3761 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Www.vg.no"
+    },
+    "author": "Tor Martin Bøe",
+    "title": "TV-anmeldelse «Hype House»: Snork, vi er i influencerbransjen",
+    "description": "TikTok-kjendiser samlet i millionvilla der de kjeder seg. Nei, det er ikke satire.",
+    "url": "https://www.vg.no/rampelys/tv/i/ALkML3/tv-anmeldelse-hype-house-snork-vi-er-i-influencerbransjen",
+    "urlToImage": "https://akamai.vgc.no/v2/images/f48fb03b-f3ef-4882-93d5-c42d22500c3c?fit=crop&format=auto&h=1069&w=1900&s=1ee6176ef6f60ab0f7957a9e311a480f2f47bced",
+    "publishedAt": "2022-01-12T16:00:00Z",
+    "content": "THIS IS THE HAPPY HOUSE: «Hype House»-gjengen i et lykkelig limousin-øyeblikk. Foto: NETFLIX\r\nTikTok-kjendiser samlet i millionvilla der de kjeder seg. Nei, det er ikke satire.\r\n«Hype House»Amerikans… [+2990 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Www.bnr.nl"
+    },
+    "author": "Herbert Blankesteijn, BNR Nieuwsradio",
+    "title": "Hoe is ons digitale lees-, kijk- en luistergedrag veilig te meten?",
+    "description": "Het Nationaal Media Onderzoek wil bijhouden hoe we lezen, kijken en luisteren. Dat werkt middels een app en speciale verbinding, mits je je daarvoor hebt aangemeld. Maar hoe kan dat op een betrouwbare, doch ook privacyvriendelijke manier? En: zo krijgen Apple…",
+    "url": "https://www.bnr.nl/podcast/digitaal/10464592/hoe-is-ons-digitale-lees-kijk-en-luistergedrag-veilig-te-meten",
+    "urlToImage": "https://bnr-external-prod.imgix.net/3ae1dd7e6e112a8fd907c6f876cea4cb4c7b1775.jpg?auto=format%252ccompress&cs=tinysrgb",
+    "publishedAt": "2022-01-12T15:56:42Z",
+    "content": "Het Nationaal Media Onderzoek wil bijhouden hoe we lezen, kijken en luisteren. Dat werkt middels een app en speciale verbinding, mits je je daarvoor hebt aangemeld. Maar hoe kan dat op een betrouwbar… [+925 chars]"
+    },
+    {
+    "source": {
+    "id": "lenta",
+    "name": "Lenta"
+    },
+    "author": "Варвара Кошечкина",
+    "title": "Россияне обратились с просьбой к Илону Маску и получили ответ",
+    "description": "Российские владельцы автомобилей Tesla записали видеообращение к американскому предпринимателю и основателю компании Илону Маску. Автовладельцы пожаловались Маску на отсутствие сервисной поддержки. В связи с этим они попросили основателя Tesla открыть официал…",
+    "url": "https://lenta.ru/news/2022/01/12/tesla/",
+    "urlToImage": "https://icdn.lenta.ru/images/2022/01/12/18/20220112184946328/share_27cf4177da57a1b3e06261455f00387f.jpg",
+    "publishedAt": "2022-01-12T15:55:00Z",
+    "content": "Tesla . YouTube.\r\n . , , - . Tesla . , , Tesla .\r\nTwitter, , . «Tesla , . , », .\r\n , Tesla 2021 936 172 . 2020 , 499 647 . 87 ."
+    },
+    {
+    "source": {
+    "id": "focus",
+    "name": "Focus"
+    },
+    "author": "mit Agenturenmaterial",
+    "title": "Grünheide bei Berlin - Behörde verwechselt Wasserwerke - jetzt droht Tesla-Fabrik eine Unterversorgung",
+    "description": "Tesla droht ein herber Dämpfer bei der Inbetriebnahme der\nGigafactory in Grünheide. Wie bekannt wurde kam es zu einem\nVerfahrensfehler – Wasserkraftwerke wurden hierbei vertauscht. Ein\nGericht muss nun erneut entscheiden, wie viel Wasser Tesla\nentnehmen darf.…",
+    "url": "https://www.focus.de/finanzen/boerse/gruenheide-droht-tesla-in-gruenheide-wassermangel-behoerden-raeumen-fehler-im-verfahren-ein_id_37824800.html",
+    "urlToImage": "https://p6.focus.de/img/regional/berlin/id_37824487/wegen-eines-verfahrensfehlers-ist-die-wasserversorgung-der-gigafactory-von-tesla-in-gruenheide-gefaehrdet..jpg?im=Crop%3D%280%2C141%2C2048%2C1024%29%3BResize%3D%281200%2C627%29&impolicy=perceptual&quality=medium&hash=458218afee4665e4d046af66ea6d1d5ade5029e3279ad78272b1abce715976d6",
+    "publishedAt": "2022-01-12T15:54:15Z",
+    "content": "Tesla droht ein herber Dämpfer bei der Inbetriebnahme der\r\nGigafactory in Grünheide. Wie bekannt wurde kam es zu einem\r\nVerfahrensfehler Wasserkraftwerke wurden hierbei vertauscht. Ein\r\nGericht muss … [+2747 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Renewableenergyworld.com"
+    },
+    "author": "Renewable Energy World",
+    "title": "Elon Musk and Bill Walton voice their opposition to proposed California solar rule changes",
+    "description": "Elon Musk and Bill Walton voice their opposition to proposed California solar rule changes News - Renewable Energy World A vote on the residential solar rule changes is slated for January 27.",
+    "url": "https://www.renewableenergyworld.com/news/elon-musk-and-bill-walton-voice-their-opposition-to-proposed-california-solar-rule-changes/",
+    "urlToImage": "https://images.renewableenergyworld.com/wp-content/uploads/2016/05/REW_SanFranciscosCommunityChoice.jpg",
+    "publishedAt": "2022-01-12T15:53:15Z",
+    "content": "Tesla’s Elon Musk and solar evangelist Bill Walton called on California officials to reject proposed changes to the state’s residential solar rooftop rules.\r\nNot mincing words, Musk called the propos… [+5303 chars]"
+    },
+    {
+    "source": {
+    "id": "handelsblatt",
+    "name": "Handelsblatt"
+    },
+    "author": "Michael Scheppe",
+    "title": "Henkel, L’Oréal, Beiersdorf : Eigene Onlineshops rentieren sich selten – warum die Kosmetikriesen trotzdem darauf setzen",
+    "description": "Die Hersteller verkaufen immer mehr über die eigene Internetseite. Finanziell lohnt sich das oft nicht. Warum machen es die Firmen – und was haben Kunden davon?",
+    "url": "https://www.handelsblatt.com/unternehmen/handel-konsumgueter/henkel-loreal-beiersdorf-eigene-onlineshops-rentieren-sich-selten-warum-die-kosmetikriesen-trotzdem-darauf-setzen/27968136.html",
+    "urlToImage": "https://www.handelsblatt.com/images/online-shopping/27969220/3-format2003.jpg",
+    "publishedAt": "2022-01-12T15:51:43Z",
+    "content": "Düsseldorf Das Shampoo aus dem House of Schwarzkopf in der edlen schwarz-grünen Flaschegibt es nicht im Supermarktregal, sondern nur auf Schwarzkopf.de. Zwölf Euro kostet das Premiumprodukt, das Hers… [+7843 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Corriere.it"
+    },
+    "author": "Massimo Sideri",
+    "title": "I primi lampioni stradali di Edison Accesi 140 anni fa a Londra",
+    "description": "Il 12 gennaio 1882 apriva a Londra, in Holborn viaduct, la prima centrale elettrica",
+    "url": "https://www.corriere.it/economia/opinioni/22_gennaio_12/i-primi-lampioni-stradali-edison-accesi-140-anni-fa-londra-3228e034-73b1-11ec-947d-d1048d2c4770.shtml",
+    "urlToImage": "https://images2.corriereobjects.it/methode_image/socialshare/2022/01/12/2aba2d40-73ba-11ec-947d-d1048d2c4770.jpg",
+    "publishedAt": "2022-01-12T15:48:16Z",
+    "content": "Il 12 gennaio 1882 — dunque esattamente 140 anni fa — veniva inaugurata la prima centrale elettrica a carbone al mondo usata per l’illuminazione stradale. La tecnologia era quella di Thomas Edison, c… [+3687 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Digital Trends"
+    },
+    "author": "Karina Tapia",
+    "title": "Las predicciones de los expertos sobre las criptomonedas y los NFT",
+    "description": "¿Cuál es el futuro de las criptomonedas y los NFT? Algunos expertos están haciendo sus predicciones sobre estas unidades digitales del futuro.",
+    "url": "https://es.digitaltrends.com/negocios/predicciones-expertos-criptomonedas/",
+    "urlToImage": "https://icdn.dtcn.com/image/digitaltrends_es/111-bitcoin.jpg",
+    "publishedAt": "2022-01-12T15:47:55Z",
+    "content": "Durante el año pasado las criptomonedas hicieron noticia de diversas maneras. Algunos las impulsaron con el fin de crea una nueva economía, y otros la critican apuntando a la enorme huella ambiental … [+9432 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "futurezone.at"
+    },
+    "author": "futurezone.at",
+    "title": "Tesla Model S mit Facelift vorgestellt",
+    "description": "Neben neuen Scheinwerfern, Rücklichtern und einem verändertem Heck, hat das Fahrzeug auch einen CCS-Anschluss.",
+    "url": "https://futurezone.at/produkte/tesla-model-s-facelift/401869187",
+    "urlToImage": "https://image.futurezone.at/images/facebook/6651581/ms-performance-hero-desktop.jpg",
+    "publishedAt": "2022-01-12T15:44:15Z",
+    "content": "12.01.2022\r\nNeben neuen Scheinwerfern, Rücklichtern und einem verändertem Heck, hat das Fahrzeug auch einen CCS-Anschluss."
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Business Standard"
+    },
+    "author": "Bansari Mayur Kamdar and Shreyashi Sanyal | Reuters",
+    "title": "Wall Street rises as in-line inflation data calms nerves; Dow up 180 points",
+    "description": "The S&P 500 was up 26.46 points, or 0.56%, at 4,739.53 and the Nasdaq Composite was up 109.59 points, or 0.72%, at 15,263.04",
+    "url": "https://www.business-standard.com/article/international/wall-street-rises-as-in-line-inflation-data-calms-nerves-dow-up-180-points-122011201515_1.html",
+    "urlToImage": "https://bsmedia.business-standard.com/_media/bs/img/article/2021-08/27/full/1630086700-069.jpg",
+    "publishedAt": "2022-01-12T15:39:00Z",
+    "content": "US stock indexes rose on Wednesday after consumer prices data that largely met expectations eased some concerns about faster-than-expected interest rate hikes, with big technology stocks offering the… [+4035 chars]"
+    },
+    {
+    "source": {
+    "id": "reuters",
+    "name": "Reuters"
+    },
+    "author": null,
+    "title": "Ocugen, Jefferies, Immuron, Wipro - Reuters",
+    "description": "U.S. stock indexes rose on Wednesday after consumer prices data that largely met expectations eased some concerns about faster-than-expected interest rate hikes, with big technology stocks offering the biggest boost.",
+    "url": "https://www.reuters.com/markets/asia/ocugen-jefferies-immuron-wipro-2022-01-12/",
+    "urlToImage": "https://www.reuters.com/pf/resources/images/reuters/reuters-default.png?d=66",
+    "publishedAt": "2022-01-12T15:34:00Z",
+    "content": "U.S. stock indexes rose on Wednesday after consumer prices data that largely met expectations eased some concerns about faster-than-expected interest rate hikes, with big technology stocks offering t… [+2815 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Electrek"
+    },
+    "author": "Micah Toll",
+    "title": "You’ve heard of electric bike sharing – now check out electric boat sharing",
+    "description": "For some time now we’ve been following the development of the Candela C-8 electric hydrofoil boat, whose Swedish manufacturer says is the fastest electric speedboat in the world. Or at least it will be, once it makes its maiden voyage later this winter.\nHowev…",
+    "url": "https://electrek.co/2022/01/12/youve-heard-of-electric-bike-sharing-now-check-out-electric-boat-sharing/",
+    "urlToImage": "https://i1.wp.com/electrek.co/wp-content/uploads/sites/3/2021/10/candela-c-8-header2.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1",
+    "publishedAt": "2022-01-12T15:33:59Z",
+    "content": "For some time now we’ve been following the development of the Candela C-8 electric hydrofoil boat, whose Swedish manufacturer says is the fastest electric speedboat in the world. Or at least it will … [+4445 chars]"
+    },
+    {
+    "source": {
+    "id": "rt",
+    "name": "RT"
+    },
+    "author": "RT",
+    "title": "Musk hits out at California over ‘bizarre anti-environment move’",
+    "description": "Tesla CEO Elon Musk has opposed California’s solar tax initiative, accusing the state government of pushing “anti-environment” legislation. Read Full Article at RT.com",
+    "url": "https://www.rt.com/business/545812-musk-california-anti-environment-move/",
+    "urlToImage": "https://cdni.rt.com/files/2022.01/article/61def1df85f5400c4d4839a0.jpg",
+    "publishedAt": "2022-01-12T15:29:53Z",
+    "content": "Tesla CEO Elon Musk has opposed Californias solar tax initiative, accusing the state government of pushing anti-environment legislation.\r\nThe plan, put forward last month by the California Public Uti… [+1115 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Wccftech"
+    },
+    "author": "Rohail Saleem",
+    "title": "It Is Finally Happening: Shiba Inu (SHIB) Is Now Expected to List on Robinhood in February 2022",
+    "description": "Shiba Inu (SHIB), a Dogecoin-inspired decentralized cryptocurrency, is catching a fresh bid from investors today amid reports that the nascent token would be listed on the popular retail brokerage platform Robinhood in February 2022. SHIBA INU ROBINHOOD LISTI…",
+    "url": "https://wccftech.com/it-is-finally-happening-shiba-inu-shib-is-now-expected-to-list-on-robinhood-in-february-2022/",
+    "urlToImage": "https://cdn.wccftech.com/wp-content/uploads/2022/01/ShibaInuRobinhood.jpg",
+    "publishedAt": "2022-01-12T15:27:14Z",
+    "content": "Shiba Inu (SHIB), a Dogecoin-inspired decentralized cryptocurrency, is catching a fresh bid from investors today amid reports that the nascent token would be listed on the popular retail brokerage pl… [+3401 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Tom's Guide"
+    },
+    "author": "Aymeric Geoffre-Rouland",
+    "title": "Tesla Model Y : sans LiDAR, elle rate son freinage et écrase un mannequin d’enfant",
+    "description": "Sur Twitter, le P.-D.G. et analyste en chef chez Moor Insights & Strategy a dévoilé un comparatif en vidéo entre le freinage d'une Tesla Model Y dénuée de capteurs LiDAR et d'une autre voiture, qui en est équipé. Le résultat est saisissant.",
+    "url": "https://www.tomsguide.fr/tesla-model-y-sans-lidar-elle-rate-son-freinage-et-ecrase-un-mannequin-denfant/",
+    "urlToImage": "https://www.tomsguide.fr/content/uploads/sites/2/2022/01/tesla-sans-capteur-lidar.png",
+    "publishedAt": "2022-01-12T15:18:07Z",
+    "content": "Sur Twitter, le P.-D.G. et analyste en chef chez Moor Insights &amp; Strategy a dévoilé un comparatif en vidéo entre le freinage d’une Tesla Model Y dénuée de capteurs LiDAR et d’une autre voiture, q… [+2699 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Jalopnik"
+    },
+    "author": "Adam Ismail",
+    "title": "Tesla Is Inching Dangerously Close To Taking The Luxury Car Sales Throne",
+    "description": "Tesla is chipping away at the old guard as General Motors tries to learn some lessons from the new one. All that and more in The Morning Shift for Wednesday, January 12, 2022.Read more...",
+    "url": "https://jalopnik.com/tesla-is-inching-dangerously-close-to-taking-the-luxury-1848343741",
+    "urlToImage": "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/b5cc32ccd11190420c105471584c69a9.jpg",
+    "publishedAt": "2022-01-12T15:17:00Z",
+    "content": "Tesla is chipping away at the old guard as General Motors tries to learn some lessons from the new one. All that and more in The Morning Shift for Wednesday, January 12, 2022.\r\n1st Gear: It Was Alway… [+9691 chars]"
+    },
+    {
+    "source": {
+    "id": "handelsblatt",
+    "name": "Handelsblatt"
+    },
+    "author": "Angelika Ivanov, Sören Imöhl, Nils Buske",
+    "title": "Bitcoin-Kurs aktuell: US-Inflationsrate mit 7,0 Prozent auf Höchststand seit 1982 - Bitcoin-Kurs steigt",
+    "description": "Bitcoin ist die teuerste Kryptowährung der Welt. Doch der Kurs ändert sich schnell und wechselt zwischen Rekorden und Tiefschlägen. Ein Überblick ab der Gründung 2008.",
+    "url": "https://www.handelsblatt.com/finanzen/bitcoin-kurs-aktuell-us-inflationsrate-mit-7-0-prozent-auf-hoechststand-seit-1982-bitcoin-kurs-steigt/26896026.html",
+    "urlToImage": "https://www.handelsblatt.com/images/bitcoin/26944760/2-format2003.jpg",
+    "publishedAt": "2022-01-12T15:15:00Z",
+    "content": "Düsseldorf Der Bitcoin-Kurs ist hochvolatil. Hat die weltweit wichtigste Cyberdevise am 10. November einen Rekord von 68.789 Dollar pro Bitcoin erreicht, fällt der Kurs je nach Nachrichtenlage schnel… [+81707 chars]"
+    }
+    ]
+    }
